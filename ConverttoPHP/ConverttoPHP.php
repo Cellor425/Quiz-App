901c98b64a4d6ec7c6246c4cc94a3ee076e87684
Cellor425 <45540592+Cellor425@users.noreply.github.com>
2021-10-04 00:21:10 -0500

    Convert to PHP Assignment added

---
+++ ConverttoPHP/ConverttoPHP.php
@@ -0,0 +1,14 @@
+<?php
+print "<!doctype html>";
+print "<html lang='en'>";
+print "<head>";
+print 	"<meta charset='utf-8'>";
+print 	"<link rel='stylesheet' href='style.css'>";
+print 	"<title>Basic Webpage</title>";
+print "</head>";
+print "<body>";
+print 	"<h1>Michael Schneider</h1>";
+print 	"<script src='hello.js'></script>";
+print "</body>";
+print "</html>";
+?>
